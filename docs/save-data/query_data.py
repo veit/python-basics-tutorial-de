3b1f42ffc307915cb0ddbd586939ccffab04b79a
veit <kontakt@veit-schiele.de>
2021-10-31 17:20:08 +0100

    Beautify output

--- docs/save-data/query_data.py
+++ docs/save-data/query_data.py
@@ -7,7 +7,8 @@ def select_all_records_from_author(cursor, author):
     print(f"All books from {author}:")
     sql = "SELECT * FROM books WHERE author=?"
     cursor.execute(sql, [author])
-    print(cursor.fetchall())  # or use fetchone()
+    for row in cursor.execute("SELECT * FROM books ORDER BY author"):
+        print(row)
 
 def select_all_records_sorted_by_author(cursor):
     print("Listing of all books sorted by author:")
@@ -18,7 +19,7 @@ def select_using_like(cursor, text):
     print(f"All books with {text} in the title:")
     sql = f"""
     SELECT * FROM books
-    WHERE title LIKE '{text}%'"""
+    WHERE title LIKE '%{text}%'"""
     cursor.execute(sql)
     print(cursor.fetchall())
 
