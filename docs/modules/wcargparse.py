b659956c03850cd3ebe2ce265c48779b470f6e10
Veit Schiele <veit@cusy.io>
2024-11-27 19:42:56 +0100

    :pencil2: Open files with contextmanager

--- docs/modules/wcargparse.py
+++ docs/modules/wcargparse.py
@@ -11,9 +11,8 @@ def words_occur():
     args = parser.parse_args()
     file_name = args.filename
     # Open the file, read it and store its words in a list.
-    f = open(file_name, "r")
-    word_list = f.read().split()
-    f.close()
+    with open(file_name, "r") as f:
+        word_list = f.read().split()
     # Count the number of occurrences of each word in the file.
     occurs_dict = {}
     for word in word_list:
